465504b9f2911515af6aa9e5afcc55e602f36a70
eduardorodriguez <eduasinco@gmail.com>
2019-03-15 01:50:53 +0100

    connection

--- app/src/main/java/com/example/eduardorodriguez/comeaqui/ProfileFragment.java
+++ app/src/main/java/com/example/eduardorodriguez/comeaqui/ProfileFragment.java
@@ -2,18 +2,21 @@ package com.example.eduardorodriguez.comeaqui;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.*;
-import android.widget.AbsListView;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
+import android.widget.*;
 import com.mikhaellopez.circularimageview.CircularImageView;
 
 import java.util.ArrayList;
@@ -45,18 +48,99 @@ public class ProfileFragment extends Fragment {
             }
         });
 
-        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
-        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
-        ListView listview;
-        listview = view.findViewById(R.id.fragment_dds_review_listView);
 
-        ViewCompat.setNestedScrollingEnabled(listview, true);
-        ArrayList<String[]> data = new ArrayList<String[]>();
-        for (int i = 0; i < 100; i++) {
-            data.add(new String[]{"h", "h", "h", "h", "f"});
-        }
-        listview.setAdapter(new GetFoodAdapter(getActivity(), data));
+        ViewPager viewPager = view.findViewById(R.id.viewpager);
+        viewPager.setAdapter(new TestPagerAdapter(getFragmentManager()));
+
+        TabLayout tabLayout = view.findViewById(R.id.tab_layout);
+
+        tabLayout.setupWithViewPager(viewPager);
+
 
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_scrolling, menu);
+        super.onCreateOptionsMenu(menu,inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    static class TestPagerAdapter extends FragmentPagerAdapter {
+
+        public TestPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new TestFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Tab " + (position + 1);
+        }
+    }
+
+    public static class TestFragment extends Fragment {
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+            View view = inflater.inflate(R.layout.tab_page, container, false);
+
+            RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+            recyclerView.setAdapter(new TestAdapter());
+
+            return view;
+        }
+    }
+
+    static class TestAdapter extends RecyclerView.Adapter<ViewHolder> {
+
+        @Override
+        public int getItemCount() {
+            return 100;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            ((TextView) holder.itemView).setText("List Item " + (position + 1));
+        }
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
     }
 }
