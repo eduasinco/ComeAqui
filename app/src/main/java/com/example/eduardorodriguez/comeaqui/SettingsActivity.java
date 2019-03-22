ea3d6cb90c2d6034a6742ed5527befbce6050705
eduardorodriguez <eduasinco@gmail.com>
2019-03-22 09:07:03 +0100

    connection

--- app/src/main/java/com/example/eduardorodriguez/comeaqui/SettingsActivity.java
+++ app/src/main/java/com/example/eduardorodriguez/comeaqui/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -11,6 +12,7 @@ import android.view.View;
 
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -32,6 +34,9 @@ public class SettingsActivity extends AppCompatActivity {
     static String location;
     static String profilePhoto;
 
+    private static SeekBar deliverRadiousSeekbarView;
+    private static int delivery_radious;
+    static long last_text_edit = 0;
 
 
     public static void setProfile(String jsonString){
@@ -45,7 +50,9 @@ public class SettingsActivity extends AppCompatActivity {
         phoneNumber = jo.get("phone_number").getAsString();
         location = jo.get("location").getAsString();
         profilePhoto = jo.get("profile_photo").getAsString();
+        delivery_radious = jo.get("deliver_radius").getAsInt();
 
+        deliverRadiousSeekbarView.setProgress(delivery_radious);
         addressView.setText(location);
     }
 
@@ -60,6 +67,8 @@ public class SettingsActivity extends AppCompatActivity {
 
         Button editAccountView = findViewById(R.id.editAccount);
         Button saveButtonView = findViewById(R.id.saveButton);
+        deliverRadiousSeekbarView = findViewById(R.id.deliverRadiousSeekbar);
+        final TextView metersTextView = findViewById(R.id.metersText);
 
 
         editAccountView.setOnClickListener(new View.OnClickListener() {
@@ -75,6 +84,27 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+        deliverRadiousSeekbarView.setProgress(delivery_radious);
+        deliverRadiousSeekbarView.setMax(1000);
+        deliverRadiousSeekbarView.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
+                delivery_radious = progress;
+                metersTextView.setText(delivery_radious + "m");
+            }
+        });
+
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.container, new PlacesAutocompleteFragment())
                 .commit();
@@ -84,6 +114,9 @@ public class SettingsActivity extends AppCompatActivity {
             public void onClick(View v) {
                 PatchAsyncTask putTast = new PatchAsyncTask();
                 putTast.execute("location", addressView.getText().toString());
+                PatchAsyncTask putTast2 = new PatchAsyncTask();
+                putTast2.execute("deliver_radius", Integer.toString(delivery_radious));
+
                 Intent k = new Intent(SettingsActivity.this, MainActivity.class);
                 k.putExtra("profile", "profile");
                 startActivity(k);
@@ -105,16 +138,41 @@ public class SettingsActivity extends AppCompatActivity {
         });
 
         addressView = findViewById(R.id.address);
-        addressView.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void afterTextChanged(Editable s) {}
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                GoogleAPIAsyncTask gAPI = new GoogleAPIAsyncTask(addressView.getText().toString());
-                gAPI.execute();
+        final long delay = 1000; // 1 seconds after user stops typing
+        final Handler handler = new Handler();
+        final Runnable input_finish_checker = new Runnable() {
+            public void run() {
+                if (System.currentTimeMillis() > (last_text_edit + delay - 500)) {
+                    GoogleAPIAsyncTask gAPI = new GoogleAPIAsyncTask(addressView.getText().toString());
+                    gAPI.execute();
+                }
             }
-        });
+        };
+
+        addressView.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged (CharSequence s,int start, int count,
+                                               int after){
+                }
+                @Override
+                public void onTextChanged ( final CharSequence s, int start, int before,
+                                            int count){
+                    //You need to remove this to run only once
+                    handler.removeCallbacks(input_finish_checker);
+
+                }
+                @Override
+                public void afterTextChanged ( final Editable s){
+                    //avoid triggering event when text is empty
+                    if (s.length() > 0) {
+                        last_text_edit = System.currentTimeMillis();
+                        handler.postDelayed(input_finish_checker, delay);
+                    } else {
+
+                    }
+                }
+            }
+
+        );
     }
 }
