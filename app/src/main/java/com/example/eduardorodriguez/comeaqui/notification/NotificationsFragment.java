2ae47e161f1878bcfd30a92dee573e821ae654d4
eduardorodriguez <eduasinco@gmail.com>
2019-09-23 11:55:05 -0700

    swipe functionality in notification

--- app/src/main/java/com/example/eduardorodriguez/comeaqui/notification/NotificationsFragment.java
+++ app/src/main/java/com/example/eduardorodriguez/comeaqui/notification/NotificationsFragment.java
@@ -3,14 +3,19 @@ package com.example.eduardorodriguez.comeaqui.notification;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -36,6 +41,10 @@ public class NotificationsFragment extends Fragment {
     ColorDrawable swipeBackgroundCancel= new ColorDrawable(Color.parseColor("#FF0000"));
     Drawable deleteIcon;
     Drawable confirmIcon;
+
+    RecyclerView recyclerView;
+    SwipeController swipeController = null;
+
 
     public NotificationsFragment() {
     }
@@ -65,17 +74,42 @@ public class NotificationsFragment extends Fragment {
         notificationAdapter = new MyNotificationsRecyclerViewAdapter(getContext(), data);
 
         Context context = view.getContext();
-        RecyclerView recyclerView = (RecyclerView) view;
+        recyclerView = (RecyclerView) view;
 
-        recyclerView.addItemDecoration(new DividerItemDecoration(this.getActivity(), LinearLayout.VERTICAL));
-        getData();
+
 
         deleteIcon = view.getContext().getDrawable(R.drawable.cancel);
         confirmIcon = view.getContext().getDrawable(R.drawable.confirm);
-        new ItemTouchHelper(simpleCallback).attachToRecyclerView(recyclerView);
-        recyclerView.setAdapter(notificationAdapter);
+
+        getData();
+        setupRecyclerView();
 
         return view;
+    }
+
+    private void setupRecyclerView() {
+        recyclerView.setAdapter(notificationAdapter);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(notificationAdapter);
+
+        swipeController = new SwipeController(new SwipeControllerActions() {
+            @Override
+            public void onRightClicked(int position) {
+                notificationAdapter.notifyItemRemoved(position);
+                notificationAdapter.notifyItemRangeChanged(position, notificationAdapter.getItemCount());
+            }
+        });
+
+        ItemTouchHelper itemTouchhelper = new ItemTouchHelper(swipeController);
+        itemTouchhelper.attachToRecyclerView(recyclerView);
+
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+                swipeController.onDraw(c);
+            }
+        });
     }
 
     void getData(){
@@ -102,73 +136,4 @@ public class NotificationsFragment extends Fragment {
         }
         notificationAdapter.notifyDataSetChanged();
     }
-
-    ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0,   ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT) {
-        @Override
-        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
-            return false;
-        }
-
-        @Override
-        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-
-
-            if (direction == ItemTouchHelper.RIGHT){
-                viewHolder.itemView.setAlpha(0);
-                viewHolder.itemView.setBackgroundColor(Color.parseColor("#FFD0FFD2"));
-                viewHolder.itemView.animate().alpha(1).x(0).setDuration(200).withEndAction(() -> {
-                    confirmOrder(data.get(viewHolder.getAdapterPosition()).order, true, getContext());
-                });
-            } else {
-                viewHolder.itemView.setAlpha(0);
-                viewHolder.itemView.setBackgroundColor(Color.parseColor("#FFD3D2"));
-                viewHolder.itemView.animate().alpha(1).x(0).setDuration(200).withEndAction(() -> {
-                    confirmOrder(data.get(viewHolder.getAdapterPosition()).order, false, getContext());
-                });
-            }
-        }
-
-        int getMargin(int margin, int rise, int down, int max_margin, int start_margin, float dX){
-            int from = 200;
-            if (from >= dX && dX > 0){
-                margin = start_margin;
-            }else if (rise >= dX && dX > from){
-                margin = start_margin - (int) ((dX - from) * (start_margin - max_margin)/(rise - from));
-            } else if (rise + down >= dX && dX > rise){
-                margin = max_margin + (int) ((dX - rise) * (margin - max_margin)/down);
-            }
-            return margin;
-        }
-
-        @Override
-        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
-            View itemView = viewHolder.itemView;
-            int iconMargin = (itemView.getHeight() - deleteIcon.getIntrinsicHeight()) / 2;
-            int margin = getMargin(210, 300, 50, 180, 230, Math.abs(dX));
-            System.out.println(margin);
-            if (dX > 0){
-                swipeBackgroundConfirm.setBounds(itemView.getLeft(), itemView.getTop(), (int) dX, itemView.getBottom());
-                confirmIcon.setBounds(itemView.getLeft() + iconMargin + margin, itemView.getTop() + iconMargin + margin, itemView.getLeft() + iconMargin + deleteIcon.getIntrinsicWidth() - margin,
-                        itemView.getBottom() - iconMargin - margin);
-                swipeBackgroundConfirm.draw(c);
-            } else {
-                swipeBackgroundCancel.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
-                deleteIcon.setBounds(itemView.getRight() - iconMargin - deleteIcon.getIntrinsicWidth() + margin, itemView.getTop() + iconMargin + margin, itemView.getRight() - iconMargin - margin,
-                        itemView.getBottom() - iconMargin - margin);
-                swipeBackgroundCancel.draw(c);
-
-            }
-            c.save();
-            if (dX > 0){
-                c.clipRect(itemView.getLeft(), itemView.getTop(), (int) dX, itemView.getBottom());
-                confirmIcon.draw(c);
-            } else {
-                c.clipRect(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
-                deleteIcon.draw(c);
-            }
-            c.restore();
-            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
-        }
-    };
-
 }
---
+++ app/src/main/java/com/example/eduardorodriguez/comeaqui/notification/SwipeControllerActions.java
@@ -0,0 +1,9 @@
+package com.example.eduardorodriguez.comeaqui.notification;
+
+public abstract class SwipeControllerActions {
+
+    public void onLeftClicked(int position) {}
+
+    public void onRightClicked(int position) {}
+
+}
